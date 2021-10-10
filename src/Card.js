6c9acfc778faa1a2881c708f9506a374f4087c0d
puramjyothirmai1 <jyothyrmai.puram@gmail.com>
2021-10-10 13:47:51 +0530

    3rd

--- src/Card.js
+++ src/Card.js
@@ -4,25 +4,25 @@ import './Card.css'
 //import {Button} from "react-bootstrap"
 //import Card from '@mui/material/Card';
 function Cards() {
-    const [text,setText]=useState("connect");
-    const changeText=(text)=>setText(text);
+    const [text, setText] = useState("connect");
+    const changeText = (text) => setText(text);
     return (
         <div className="card-container">
-  
+
             {
-           data.map(dat=>
-           <>
-            <div className="cards">
-            <img src={dat.img}/>
-                <h1>{dat.name}</h1>
-<p>{dat.email}</p>
-<button onClick={()=>changeText("request Sent")}>{text}</button>
-</div>
-</>
-              )
-}
+                data.map(dat =>
+                    <>
+                        <div className="cards">
+                            <img src={dat.img} />
+                            <h1>{dat.name}</h1>
+                            <p>{dat.email}</p>
+                            <button onClick={() => changeText("request Sent")}>{text}</button>
+                        </div>
+                    </>
+                )
+            }
         </div>
     )
 }
 
-export default Cards
+export default Cards;
