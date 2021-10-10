acfd9813bb686df65942128fa2c945652b4f7fc9
puramjyothirmai1 <jyothyrmai.puram@gmail.com>
2021-10-10 18:31:19 +0530

    before out

--- src/Card.js
+++ src/Card.js
@@ -1,22 +1,28 @@
 import React, { useState } from 'react'
 import data from './data.json'
 import './Card.css'
-//import {Button} from "react-bootstrap"
-//import Card from '@mui/material/Card';
 function Cards() {
     const [text, setText] = useState("connect");
     const changeText = (text) => setText(text);
+    const remove = (id) => {
+        alert("removig" + id);
+    }
+
     return (
-        <div className="card-container">
+        <div className="card-container" >
 
             {
                 data.map(dat =>
                     <>
+
                         <div className="cards">
+                            <button onClick={() => remove(dat.id)}>remove {dat.id} </button>
+
                             <img src={dat.img} />
                             <h1>{dat.name}</h1>
                             <p>{dat.email}</p>
                             <button onClick={() => changeText("request Sent")}>{text}</button>
+
                         </div>
                     </>
                 )
